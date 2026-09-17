d0efa11100d8be5402a4a98d6d2b4b9e95dfa2a5
rdvprasad <rdvprasad9@gmail.com>
2026-09-17 06:50:23 +0000

    rool back and commited

--- app.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask, render_template, request, jsonify
-
-@app.route('/todo')
-def todo_page():
-    return render_template('todo.html')
---
+++ templets/app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+import pymongo
+
+MONGO_URI = os.getenv('MONGO_URI')
+
+# MongoDB Connection
+client = pymongo.MongoClient('MONGO_URI')
+client = MongoClient
+db = client["todo_db"]
+todos_collection = db["todos"]
+
+app = Flask(__name__)
+
+
+@app.route('/todo')
+def todo_page():
+    return render_template('todo.html')
+
+
+@app.route('/submittodoitem', methods=['POST'])
+def submit_todo_item():
+    if request.is_json:
+        data = request.get_json()
+        item_name = data.get('itemName')
+        item_description = data.get('itemDescription')
+    else:
+        item_name = request.form.get('itemName')
+        item_description = request.form.get('itemDescription')
+
+    if not item_name or not item_description:
+        return jsonify({"error": "itemName and itemDescription are required"}), 400
+
+    new_item = {
+        "itemName": item_name,
+        "itemDescription": item_description
+    }
+    result = todos_collection.insert_one(new_item)
+
+    return jsonify({
+        "message": "Item stored successfully",
+        "id": str(result.inserted_id)
+    }), 201
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
